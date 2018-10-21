1980aa32f22dbc9d47feaa27699198111f421ba4
JCPosso <44146884+JCPosso@users.noreply.github.com>
2018-10-20 19:58:39 -0500

    Update IemoisException.java

--- IemoisException.java
+++ IemoisException.java
@@ -4,6 +4,8 @@ public class IemoisException extends Exception{
     public static final String  DISTRIBUIDOR_DESCONOCIDO= "El Distribuidor no es conocido";
 	public static final String  CURSO_YA_EXISTE= "El curso ya existe";
 	public static final String  INFORMACION_DESCONOCIDA= "Es necesario poner informacion sobre el curso";
+	public static final String  CURSO_NO_ENCONTRADO= "Curso no encontrado";
+
     public IemoisException(String message){
 		super(message);
 	}
